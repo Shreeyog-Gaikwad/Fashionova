e1ffe88a49034c64dcf34f6ef34df7fcb1e5af3e
Shreeyog Gaikwad <shreeyoggaikwad@gmail.com>
2024-06-28 12:39:29 +0530

    Changes made in Shopcontext

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -48,6 +48,8 @@ const ShopContextProvider = (props) =>{
         return totalItem;
     }
 
+    console.log(cartItems);
+
     const contextValue = {all_products, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalItems};
 
     return(
