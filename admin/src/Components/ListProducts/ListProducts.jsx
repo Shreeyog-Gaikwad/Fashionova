2a9046120f8771c24e2b5df97a7c570423b34509
Shreeyog-Gaikwad <shreeyoggaikwad@gmail.com>
2024-07-12 11:23:03 +0530

    Admin Panel Add Products Component Added

---
+++ admin/src/Components/ListProducts/ListProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ListProducts.css"
+
+const ListProducts = () => {
+  return (
+    <div className='list-products'>
+      
+    </div>
+  )
+}
+
+export default ListProducts
--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 import "./Admin.css";
-import Sidebar from '../../Components/Sidebar/Sidebar';
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import { Routes, Route } from 'react-router-dom'
+import AddProducts from "/9) Projects/Fashionova (E-Commerce)/admin/src/Components/AddProducts/AddProducts"
+import ListProducts from "../../Components/ListProducts/ListProducts"
 
 const Admin = () => {
   return (
-    <div className='admin'>
-      <Sidebar/>
+    <div className="admin">
+      <Sidebar />
+      <Routes>
+        <Route path="/addproduct" element={<AddProducts/>} />
+        <Route path="/listproduct" element={<ListProducts/>} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
