d409b7f3dc07f1cd7bd2a7d2a8ab1f3f68d5ca47
Shreeyog-Gaikwad <shreeyoggaikwad@gmail.com>
2024-07-13 16:14:06 +0530

    Login and SignUp API Endpoint Added

--- backend/index.js
+++ backend/index.js
@@ -7,131 +7,210 @@ const cors = require("cors");
 const path = require("path");
 const { type } = require("os");
 const a = require("./target");
+const { error } = require("console");
 
 const app = express();
 
 // All request will be parsed into Json.
-app.use(express.json()); 
+app.use(express.json());
 
 // Used to connect project to 4000 port.
-app.use(cors());  
+app.use(cors());
 
 // Database Connectivity by Mongodb
 mongoose.connect(`mongodb+srv://${a}@cluster0.hpmr3zg.mongodb.net/Fashionova`)
 
 //API creation
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("Express App is Running");
 })
 
 
 // Image storage engine 
 const storage = multer.diskStorage({
-    destination : './upload/images',
-    filename:(req, file, cb)=>{
+    destination: './upload/images',
+    filename: (req, file, cb) => {
         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
 })
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 
 // API endpoint for Image Upload
 app.use("/images", express.static('upload/images'))
-app.post('/upload', upload.single('product'), function(req,res){
+app.post('/upload', upload.single('product'), function (req, res) {
     res.json({
-        success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
     })
 })
 
 
 // Schema for Creating Products
 const Product = mongoose.model("Product", {
-    id:{
+    id: {
         type: Number,
         required: true,
     },
-    name :{
-        type : String,
-        required : true
-    } ,
-    image : {
-        type : String,
-        required :true
+    name: {
+        type: String,
+        required: true
     },
-    category : {
-        type : String,
-        required :true
+    image: {
+        type: String,
+        required: true
     },
-    new_price :{
-        type : Number,
-        required:true
+    category: {
+        type: String,
+        required: true
     },
-    old_price :{
-        type : Number,
-        required :true
+    new_price: {
+        type: Number,
+        required: true
     },
-    date :{
-        type : Date,
-        default : Date.now
+    old_price: {
+        type: Number,
+        required: true
     },
-    available :{
-        type :Boolean,
-        default :true
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    available: {
+        type: Boolean,
+        default: true
     }
 })
 
 // API endpoint for Adding Product to Database
-app.post('/addproduct', async function(req,res){
+app.post('/addproduct', async function (req, res) {
     let products = await Product.find({});
     let id;
-    if(products.length>0){
+    if (products.length > 0) {
         let last_product_array = products.slice(-1);
         let last_product = last_product_array[0];
-        id = last_product.id +1;
+        id = last_product.id + 1;
     }
-    else{
-        id=1;
+    else {
+        id = 1;
     }
     const product = new Product({
-        id : id,
-        name : req.body.name,
-        image : req.body.image,
-        category : req.body.category,
-        new_price : req.body.new_price,
-        old_price : req.body.old_price,
+        id: id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
     });
     console.log(product);
     await product.save();
     console.log("Saved");
     res.json({
-        success:true,
-        name:req.body.name
+        success: true,
+        name: req.body.name
     })
 })
 
 // API endpoint for Removing Product from Database
-app.post("/removeproduct" , async function(req,res){
-    await Product.findOneAndDelete({id:req.body.id});
+app.post("/removeproduct", async function (req, res) {
+    await Product.findOneAndDelete({ id: req.body.id });
     console.log("Product Removed");
     res.json({
-        success:true,
-        name:req.body.name
+        success: true,
+        name: req.body.name
     })
 })
 
 // API endpoint for getting All Products from Database
-app.get('/allproducts', async function(req, res){
+app.get('/allproducts', async function (req, res) {
     let products = await Product.find({});
     console.log("All Products Fetched...");
     res.json(products);
 })
 
 
-app.listen(port, (error)=>{
-    if(!error){
-        console.log("Server Running on Port "+port);
-    } 
+// Schema for Users
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// API Endpoint for registering the user
+app.post('/signup', async function (req, res) {
+    let check = await Users.findOne({ email: req.body.email });
+    if (check) {
+        return res.status(400).json({ success: false, errors: "Existing user found with same Email address..." });
+    }
+
+    let cart = {};
+
+    for (let index = 0; index < 300; index++) {
+        cart[index] = 0;
+    }
+
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    });
+
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({ success: true, token });
+
+});
+
+// API Endpoint for User Login
+app.post('/login', async function (req, res) {
+    let user = await Users.findOne({ email: req.body.email });
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data= {
+                user : {
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data, "secret_ecom");
+            res.json({success:true, token});
+        }
+        else{
+            res.json({success:false,errors:"Wrong Password!!"});
+        }
+    }
     else{
-        console.log("Error : "+ error);
+        res.json({success:false, errors:"Wrong Email Id!!"});
+    }
+})
+
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port " + port);
+    }
+    else {
+        console.log("Error : " + error);
     }
 })
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,25 +1,134 @@
-import React from 'react'
-import "./CSS/LoginSignup.css"
+import React, { useState } from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+
+  const [formData, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const changehandler = (e) => {
+    setForm({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const signUp = async () => {
+    console.log("Sign Up", formData);
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
+  };
+
+  const login = async () => {
+    console.log("Login", formData);
+    let responseData;
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
+  };
+
   return (
-    <div className='loginsignup'>
+    <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" name="" id="" placeholder='Your Name'/>
-          <input type="email" name="" id="" placeholder='Email Address'/>
-          <input type="password" name="" id="" placeholder='Password'/>
+          {state === "Sign Up" ? (
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={changehandler}
+              id=""
+              placeholder="Your Name"
+            />
+          ) : (
+            <></>
+          )}
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={changehandler}
+            id=""
+            placeholder="Email Address"
+          />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={changehandler}
+            id=""
+            placeholder="Password"
+          />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <button
+          onClick={() => {
+            state === "Login" ? login() : signUp();
+          }}
+        >
+          Continue
+        </button>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account?{" "}
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account?{" "}
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              Sign Up
+            </span>
+          </p>
+        )}
         <div className="loginsignup-agree">
-          <input type="checkbox" name='' id='' />
-          <p>By continuing,  I agree to the terms of use & privacy policy</p>
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
